f09ffb69174ae19e488be18671902945304bdbb1
MartinRo <martin@rodemann.org>
2015-04-14 09:56:15 +0200

    readd missing vzw product file
    Change-Id: Ic01778b919ebfb44eff18c396a4a41f00492ad76

---
+++ products/vzw.mk
@@ -0,0 +1,22 @@
+#Permissions and Lib for Verizon Wireless Apps based on Unified devices
+#GMS Client ID is only set on devices that are compiled specifically for vzw I.E. d2vzw not d2lte
+
+ifeq ($(VZW_UNIFIED),true)
+PRODUCT_COPY_FILES += \
+    vendor/exodus/proprietary/vzw/app/VerizonSSO.apk:system/blobs/vzw/app/VerizonSSO.apk \
+    vendor/exodus/proprietary/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/blobs/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml \
+    vendor/exodus/proprietary/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/blobs/vzw/etc/permissions/com.vzw.hardware.lte.xml \
+    vendor/exodus/proprietary/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/blobs/vzw/etc/permissions/com.vzw.vzwapnlib.xml \
+    vendor/exodus/proprietary/vzw/lib/libmotricity.so:system/blobs/vzw/lib/libmotricity.so
+else
+PRODUCT_PACKAGES += \
+    VerizonSSO
+
+PRODUCT_COPY_FILES += \
+    vendor/exodus/proprietary/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
+    vendor/exodus/proprietary/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/etc/permissions/com.vzw.hardware.lte.xml \
+    vendor/exodus/proprietary/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \
+    vendor/exodus/proprietary/vzw/lib/libmotricity.so:system/lib/libmotricity.so
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
+endif
