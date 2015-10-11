cef0fd4697146e798db0112bba75b0f8ada1184d
MartinRo <martin@rodemann.org>
2015-10-11 16:29:07 +0000

    Add overlay props
    Change-Id: I4b92adaf467dc464c1073ebf9577896307a23450

--- config/common.mk
+++ config/common.mk
@@ -37,6 +37,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+PRODUCT_PACKAGE_OVERLAYS += vendor/exodus/overlay/common
+
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
