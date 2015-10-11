3f490ffe2a7482d02610ef80409298da69ef3a17
MartinRo <martin@rodemann.org>
2015-10-11 10:25:47 +0000

    Add some props
    Change-Id: Iaf29ae47d3ba096c4864f16d7e445d35bc487624

--- config/common.mk
+++ config/common.mk
@@ -28,3 +28,23 @@ PRODUCT_BOOTANIMATION := vendor/exodus/prebuilt/bootanimation/$(TARGET_BOOTANIMA
 PRODUCT_COPY_FILES += \
 	vendor/exodus/prebuilt/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
 endif
+
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dataroaming=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+	
