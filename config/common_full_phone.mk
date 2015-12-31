91a0a5f733fc874b537670c3467051d8a5995f67
Raja Mungamuri <Raja1612.M@gmail.com>
2015-12-31 18:28:44 +0530

    Dex pre optimization enabled to reduce apps compile & speed launch
    As per ART documentation
    
    - App optimize of installed is enough no need to optimized pre installed apps.
    - Speed specs will compile the apps bit late but outcome is stable and speed performance.
    - As per dave suggestions added ? to precheck for empty.
    
    Change-Id: If7724ba0a4585a34a7eae48e7dfd4a9a5aab8b42

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -8,10 +8,22 @@ PRODUCT_PACKAGES += \
 # Include Exodus LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/exodus/overlay/dictionaries
 
-# Default notification/alarm sounds
+
+# Enable dexpreopt to reduce no. of app optimization & time.
+ART_BUILD_TARGET_DEBUG := true
+ART_BUILD_HOST_DEBUG := true
+WITH_DEXPREOPT ?= true
+
+# dex2oat which result in all user apps running in the interpreted mode
+PRODUCT_PROPERTY_OVERRIDES += \
+     dalvik.vm.dex2oat-filter=interpret-only \
+     dalvik.vm.image-dex2oat-filter=speed
+
+# Default notification/alarm/ringtone sounds
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Helium.ogg
+    ro.config.alarm_alert=Helium.ogg \
+    ro.config.ringtone=Themos.ogg
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
