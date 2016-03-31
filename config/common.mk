BlissRoms/platform_vendor_bliss

vendor/aosp: AOSP keyboard swype compatibility
*also differentiate libs based on device for arm/arm64

common.mk
@@ -103,6 +103,15 @@ PRODUCT_PACKAGE_OVERLAYS += \
 	vendor/aosp/overlay/common \
 	vendor/aosp/overlay/dictionaries
 
+# Proprietary latinime libs needed for Keyboard swyping
+ifneq ($(filter arm64,$(TARGET_ARCH)),)
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
+endif
+
 # by default, do not update the recovery with system updates
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 

TeamExodus/vendor_exodus

Change-Id: I53805df7390273b4e5a4661fbd80bda5baf4cf58

config/common.mk
@@ -327,8 +327,19 @@ else
 WITH_DEXPREOPT := false
 endif
 
-# Include LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/exodus/overlay/dictionaries
+# Overlays & Include LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += \
+	vendor/exodus/overlay/common \
+	vendor/exodus/overlay/dictionaries
+
+# Proprietary latinime libs needed for Keyboard swyping
+ifneq ($(filter arm64,$(TARGET_ARCH)),)
+PRODUCT_COPY_FILES += \
+    vendor/exodus/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
+else
+PRODUCT_COPY_FILES += \
+    vendor/exodus/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so
+endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.exodus.display.version=$(EXODUS_DISPLAY_VERSION)